7da7dd8a975fd6e7dddfae9315b315192dd493ff
Baha eddine Ellouze <bahaaellouze@gmail.com>
2025-04-20 13:13:06 +0100

    Setup User authentication system

--- Backend/middleware/errorHandler.js
+++ Backend/middleware/errorHandler.js
@@ -1,4 +1,3 @@
-// middleware/errorHandler.js
 const errorHandler = (err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!', error: err.message });
--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,22 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-// Import config, routes, and middleware
 const connectDB = require('./config/db');
-const itemRoutes = require('./routes/itemRoutes');
+const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
 connectDB();
 
-// API Routes
-app.use('/api/items', itemRoutes);
-
-// Error Handling Middleware
+app.use('/api/auth', authRoutes);
 app.use(errorHandler);
 
-// Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
